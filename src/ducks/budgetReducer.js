c12fb1db5da32238f2a4eb31fc261b2358fa0848
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-09 15:50:56 -0800

    reducer;

--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -7,7 +7,8 @@ const initialState = {
 }
 const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA';
 
-const ADD_PURCHASE = 'ADD_PURCHASE'
+const ADD_PURCHASE = 'ADD_PURCHASE';
+const REMOVE_PURCHASE = 'REMOVE_PURCHASE';
 
 export const requestBudgetData = () => {
   let data = axios.get('/api/budget-data').then(res => res.data)
@@ -15,6 +16,26 @@ export const requestBudgetData = () => {
     type: REQUEST_BUDGET_DATA,
     payload: data
   }
+}
+
+export const addPurchase = (price, description, category) => {
+  let data = axios.post('/api/budget-data/purchase', {
+    description,
+    price,
+    category
+  }).then(res => res.data);
+  return {
+    type: ADD_PURCHASE,
+    payload:data
+  }
+}
+
+export const removePurchase = (id) => {
+    let data = axios.delete(`/api/budget-data/purchase/${id}`).then(res => res.data);
+    return {
+      type: REMOVE_PURCHASE,
+      payload: data
+    }  
 }
 
 export default function budgetReducer(state = initialState, action) {
