7377d9c70eb9c5b55c09da4573000ed7e8a321bc
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-09 15:25:04 -0800

    hey that works!

--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -1,32 +1,30 @@
 import axios from 'axios';
 
-
-const REQUEST_BUDGET_DATA = 'REQUEST_BODY_DATA';
-
-
 const initialState = {
   purchases: [],
   budgetLimit: null,
   loading: false
 }
+const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA';
+
+const ADD_PURCHASE = 'ADD_PURCHASE'
 
 export const requestBudgetData = () => {
   let data = axios.get('/api/budget-data').then(res => res.data)
   return{
     type: REQUEST_BUDGET_DATA,
-    payload:data
+    payload: data
   }
 }
 
-export default function reducer(state = initialState, action){
-  switch (action.type){
+export default function budgetReducer(state = initialState, action) {
+  switch (action.type) {
     case REQUEST_BUDGET_DATA + '_PENDING':
-      return {...state, loading:true}
-    case 
-      REQUEST_BUDGET_DATA + '_FULFILLED':
-        return {...state, ...action.paylod, loading:false}
-      default:
-        return state;
+      return { ...state, loading: true }
+    case REQUEST_BUDGET_DATA + '_FULFILLED':
+      return { ...state, ...action.payload, loading: false }
+    default:
+      return state;
   }
 }
 
